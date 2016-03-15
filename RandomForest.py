8613a8be53b05df95ca71b1b85cd3a95d2391712
JeremyIV <Cesiumlifejacket@gmail.com>
2016-03-14 21:02:14 -0700

    first working random forest; overfitting

--- RandomForest.py
+++ RandomForest.py
@@ -1,40 +1,42 @@
-from sklearn import ensemble 
+from sklearn import ensemble
 from sklearn.cross_validation import train_test_split
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 
-
+# read in and format data
 def strToFloat(data):
-	stringNums = {}
-	i = 0
-	for d in data:
-		if d not in stringNums:
-			stringNums[d] = i
-			i += 1
+    stringNums = {string: i for i, string in enumerate(set(data))}
+    return np.array([stringNums[d] for d in data])
 
-	return [stringNums[d] for d in data]
+raw_df = pd.read_csv("data.csv")
+df = pd.DataFrame()
 
-df = pd.read_csv("data.csv")
+# convert strings to integer classes
+for col in raw_df.columns:
+    if raw_df[col].dtype == np.dtype('O'):
+        df[col] = strToFloat(raw_df[col])
+    else:
+        df[col] = raw_df[col]
 
-train, test = train_test_split(df, test_size = 0.2)
+del raw_df
 
-print train.shape
-print test.shape
+# replace NaNs (missing values) with -1
+df[np.isnan(df)] = -1
 
-attribute_cols = [col for col in train.columns if col not in ["GN/LS"]]
+train, test = train_test_split(df, test_size=0.2)
 
-X_train = train[attribute_cols]
-X_test = test[attribute_cols]
+y_train = train["GN/LS"]
+x_train = train
+del x_train["GN/LS"]
 
-X_train = list(map(strToFloat, X_train.columns))
-X_test = list(map(strToFloat, X_test.columns))
+y_test = test["GN/LS"]
+x_test = test
+del x_test["GN/LS"]
 
-y = train["GN/LS"]
-print(y)
-y = list(map(strToFloat, y))
+RFC = ensemble.RandomForestClassifier(n_estimators=30, n_jobs=-1)
+RFC.fit(x_train, y_train)
 
-test_ids = test["GN/LS"]
-
-RFC = ensemble.RandomForestClassifier(n_estimators = 30, n_jobs = -1)
-
-RFC.fit(X_train, y)
+predicted = RFC.predict(x_test)
+actual = np.array(y_test)
+correlation = np.corrcoef(np.array([predicted, actual]))[0, 1]
+print("Correlation: ", correlation)
