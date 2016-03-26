f2e394c5f8421202d0558a4072bed6d060ac74df
michaelzeng96 <qzeng18@my.whitworth.edu>
2016-03-26 01:58:46 -0700

    Dropped two more columns, and plotted importance of each feature
    Dropped "ODK" column and "Play #" column. Since we are not keeping track of history yet, Play # does not matter, and ODK is "O" (standing for offense) each time, so no point. 
    
    Printed out each column/feature's importance, and plotted in as well along with standard deviation.

--- RandomForest.py
+++ RandomForest.py
@@ -1,7 +1,13 @@
+
+# coding: utf-8
+
+# In[60]:
+
 from sklearn import ensemble
 from sklearn.cross_validation import train_test_split
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # read in and format data
 def strToFloat(data):
@@ -24,7 +30,6 @@ del raw_df
 df[np.isnan(df)] = -1
 
 #classify yards gained/lost
-#classify yards gained/lost
 gnls = np.array(df["GN/LS"])
 gnls[gnls <= 0] = 0
 gnls[gnls < 10] = 1
@@ -33,6 +38,12 @@ df["GN/LS"] = gnls
         
 #drop opponent team name
 df = df.drop("OPP TEAM", 1)
+
+#drop PLAY NUMBER
+df = df.drop("PLAY #", 1)
+
+#DROP ODK (not needed, plus it's importance is 0)
+df = df.drop("ODK", 1)
 
 train, test = train_test_split(df, test_size=0.2)
 
@@ -44,10 +55,55 @@ y_test = test["GN/LS"]
 x_test = test
 del x_test["GN/LS"]
 
-RFC = ensemble.RandomForestClassifier(n_estimators=10, n_jobs=-1)
+
+# In[63]:
+
+
+RFC = ensemble.RandomForestClassifier(n_estimators=1000, n_jobs=-1)
 RFC.fit(x_train, y_train)
 
 predicted = RFC.predict(x_test)
 actual = np.array(y_test)
+
+
+# In[64]:
+
 correlation = np.corrcoef(np.array([predicted, actual]))[0, 1]
 print("Correlation: ", correlation)
+
+
+# In[65]:
+
+#get list of features, ranked by importance
+importances = RFC.feature_importances_
+
+
+# In[67]:
+
+indices = np.argsort(importances)[::-1]
+
+for f in range(x_train.shape[1]):
+    print("%d. feature %d (%f)" % (f+1, indices[f], importances[indices[f]]))
+    
+#feature 3 is YARD LN, feature 7 is RESULT, feature 9 is OFF PLAY
+#feature 6 is PASS ZONE, feature 13 is COVERAGE, feature 1 is DIST
+#etc... 
+
+
+# In[ ]:
+
+std = np.std([tree.feature_importances_ for tree in RFC.estimators_],
+            axis=0)
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(x_train.shape[1]), importances[indices], color = "r",
+             yerr=std[indices], align="center")
+plt.xticks(range(x_train.shape[1]), indices)
+plt.xlim([-1, x_train.shape[1]])
+plt.show()
+
+
+# In[ ]:
+
+
+
